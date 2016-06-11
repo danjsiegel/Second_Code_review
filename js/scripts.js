a1f1383c212306ae18e8d12cb1190d1ca38a27df
danjsiegel <dan.j.siegel@gmail.com>
2016-06-11 10:34:36 -0700

    all functions working, all desired (not all possible) paths have been created for the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,38 @@
+var participants = function (parti) {
+  return (parti == null || parti == "")
+}
 var tendies = function (option1, option2, option3, option4, option5) {
   return (option1 === 'old' && option4 === 'idk');
 };
 var python = function (option1, option2, option3, option4, option5) {
-  return (option1 === "young" || option1 === "middle-aged" || option1 === "old" && option3 >= 1 && option4 !== "idk" );
-
+  return (option1 === "young" && option3 >= 1 && option4 === "idk" );
+};
+var hawaii = function (option1, option2, option3, option4, option5) {
+  return (option1 === 'old' || option1 === 'young' || option1 === 'middle-aged' && option3 >= 1 && option4 === 'african' || option4 === 'european')
 }
 $(document).ready(function() {
   $("form#vacay").submit(function() {
       event.preventDefault();
       var option1 = $("input:radio[name='agegroup']:checked").val();
       var option2 = $("#vacationtype").val();
-      var option3 = parseInt($("#number-of-people").val());
+      var parti = $("#number-of-people").val();
+      var option3 = parseInt($(parti).val());
       var option4 = $("input:radio[name='swallowgroup']:checked").val();
       var option5 = $("#firstname").val()
-      if (tendies(option1, option2, option3, option4, option5)) {
+
+      if (participants(parti)) {
+        alert("I need an age")
+      } else if (tendies(option1, option2, option3, option4, option5)) {
         $("#python").hide()
         $("#actualvacation").hide()
         $("#tendiesclass").show()
       } else if (python(option1, option2, option3, option4, option5)) {
         $("#python").show()
         $("#actualvacation").hide()
+        $("#tendiesclass").hide()
+      } else if (hawaii(option1, option2, option3, option4, option5)) {
+        $("#python").hide()
+        $("#actualvacation").show()
         $("#tendiesclass").hide()
       }
       });
